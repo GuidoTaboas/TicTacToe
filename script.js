3edac8c9d95c9d4ccd8bb2efe39d88e493d4fcaf
GuidoTaboas <guidotaboas@gmail.com>
2022-02-15 13:01:22 +0100

    Revamped the whole UI to make it look nicer

--- script.js
+++ script.js
@@ -7,20 +7,32 @@
 const GameBoard = (() => {
     let Board = [1,2,3,4,5,6,7,8,9];
     return {Board};
-})
+})()
 
-const DisplayBoard = ((GameBoard) => {
+const DisplayBoard = (() => {
     const board = document.getElementById('board');
+    
+    const display = (GameBoard) => {
     GameBoard.forEach(item => {
         div = document.createElement('div');
         div.id = item;
-        div.setAttribute('data-player', item)
+        div.setAttribute('data-player', item);
         div.classList.add('tile');
         board.appendChild(div);
-    })
-})
+    })}
 
+    const resetBoard = () => {
+        let  i = 1
+        const tiles = document.querySelectorAll('.tile');
+        tiles.forEach(item => {
+            item.setAttribute('data-player', i)
+            item.style.backgroundColor = 'transparent'
+            i++
+        })
+    }
+    return {resetBoard, display}
 
+})()
 
 const Player = (name) => {
     const showName = () => console.log(name);
@@ -51,8 +63,23 @@ const Rules = ((P1, P2, Board) => {
 
     const checkWin = (board) => {
        if (checkCol() || checkDia() || checkRow()) {
-           console.log('winnn')
+           return 'win'
+       } else if(checkTie()) {
+           console.log('tie')
        }
+    }
+
+    // if every tile data-player is not in board, means every tile is occupied
+    const checkTie = () => {
+        let board = [1,2,3,4,5,6,7,8,9]
+        let flag = false
+        tiles.forEach(item => {
+           if(board.includes(parseInt(item.getAttribute('data-player')))) {
+               flag = true
+               return 
+           }
+        return flag
+        })
     }
 
     const checkRow = () => {
@@ -64,7 +91,7 @@ const Rules = ((P1, P2, Board) => {
                     return tiles[3].getAttribute('data-player');
         } else if (tiles[6].getAttribute('data-player') === tiles[7].getAttribute('data-player') &&
                     tiles[6].getAttribute('data-player') === tiles[8].getAttribute('data-player')) {
-                     return tiles[6].getAttribute('data-player');
+                    return tiles[6].getAttribute('data-player');
         }
     }
 
@@ -74,10 +101,10 @@ const Rules = ((P1, P2, Board) => {
                 return tiles[0].getAttribute('data-player');
         } else if (tiles[1].getAttribute('data-player') === tiles[4].getAttribute('data-player') &&
                     tiles[1].getAttribute('data-player') === tiles[7].getAttribute('data-player') ) {
-                        return tiles[1].getAttribute('data-player');
+                    return tiles[1].getAttribute('data-player');
         } else if (tiles[2].getAttribute('data-player') === tiles[5].getAttribute('data-player') &&
                     tiles[2].getAttribute('data-player') === tiles[8].getAttribute('data-player')) {
-                        return tiles[2].getAttribute('data-player');
+                    return tiles[2].getAttribute('data-player');
         }
     }
 
@@ -92,24 +119,38 @@ const Rules = ((P1, P2, Board) => {
     }
 
     return {changeTurn, checkPlayer, checkWin};
-})
+})('p1', 'p2', board.Board)
 
-let board = GameBoard() ;
-DisplayBoard(board.Board);
-const rules = Rules('p1', 'p2', board.Board);
-
+DisplayBoard.display(GameBoard.Board);
 const tiles = document.querySelectorAll('.tile');
 
 tiles.forEach(item => {
     item.addEventListener('click', function() {
-        if (rules.checkPlayer() === 'p1' && item.id !== 'p2' ) {   //Aqui estaba evaluando rules.currentPlayer    
-            item.style.backgroundColor = 'red';
+        if (Rules.checkPlayer() === 'p1' && !(['p2', 'p1'].includes(item.getAttribute('data-player')))) {   //Aqui estaba evaluando rules.currentPlayer    
+            item.style.backgroundColor = '#F8E9A1';
             item.setAttribute('data-player', 'p1');
-        } else if (rules.checkPlayer() === 'p2' && item.id !== 'p1') {
-            item.style.backgroundColor = 'blue';
+            if (Rules.checkWin()) {
+                alert('Player 1 has won');
+                DisplayBoard.resetBoard()
+                return
+            }
+            Rules.changeTurn(); 
+        } else if (Rules.checkPlayer() === 'p2' && !(['p2', 'p1'].includes(item.getAttribute('data-player')))) {
+            item.style.backgroundColor = '#374785';
             item.setAttribute('data-player', 'p2');
+            if (Rules.checkWin()) {
+                alert('Player 2 has won')
+                DisplayBoard.resetBoard()
+                return
+            }
+            Rules.changeTurn(); 
         }
-        rules.checkWin();
-        rules.changeTurn() ; 
+        
     })
 });
+
+const restartButton = document.getElementById('restartButton')
+
+restartButton.addEventListener('click', function() {
+    DisplayBoard.resetBoard()
+})
