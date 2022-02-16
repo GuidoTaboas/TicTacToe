0ae37916a2b9d31fbce6eb459a7ad973e1f80e37
GuidoTaboas <guidotaboas@gmail.com>
2022-02-16 16:14:01 +0100

    Added start button and player's name selection

--- script.js
+++ script.js
@@ -5,37 +5,49 @@
 //flow: players are created. players move. check win. 
 
 const GameBoard = (() => {
-    let Board = [1,2,3,4,5,6,7,8,9];
+    let Board = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9};
     return {Board};
 })()
 
-const DisplayBoard = (() => {
-    const board = document.getElementById('board');
-    
+const Display = (() => {
+    const board1 = document.getElementById('board');
+    const boardArr = GameBoard.Board
     const display = (GameBoard) => {
-    GameBoard.forEach(item => {
-        div = document.createElement('div');
-        div.id = item;
-        div.setAttribute('data-player', item);
-        div.classList.add('tile');
-        board.appendChild(div);
-    })}
+        for(let [key] of Object.entries(GameBoard)) {
+            div = document.createElement('div');
+            div.id = key;
+            div.setAttribute('data-player', key);
+            div.classList.add('tile');
+            board1.appendChild(div);
+        }
+    }
 
     const resetBoard = () => {
         let  i = 1
         const tiles = document.querySelectorAll('.tile');
         tiles.forEach(item => {
+            boardArr[(item.id - 1)] = i
             item.setAttribute('data-player', i)
             item.style.backgroundColor = 'transparent'
             i++
         })
     }
-    return {resetBoard, display}
+
+    const endMessage = (winner) => {
+        let endMessageBox = document.getElementById('endMessage')
+        if(winner === 'p1') {
+            endMessageBox.innerText = `The Player ${document.getElementById('p1').innerText} won `
+        } else {
+            endMessageBox.innerText = `The Player ${document.getElementById('p2').innerText} won `
+        }
+        endMessageBox.style.visibility = 'visible'
+    }
+
+    return {resetBoard, display, endMessage}
 
 })()
 
 const Player = (name) => {
-    const showName = () => console.log(name);
     return {
         name
     }
@@ -45,8 +57,9 @@ const Player = (name) => {
 // en lugar de devolver, checkPlayer, estaba devolviendo currentPlayer, y evaluando abajo rules.currentPlayer
 //currentPlayer se actualizaba, pero rules.currentPlayer no se actualizaba, porque?
 
-const Rules = ((P1, P2, Board) => {
+const Rules = ((P1, P2) => {
     let currentPlayer = P1;
+    let board = GameBoard.Board
 
     const changeTurn = () => {
         if (currentPlayer === P1){
@@ -61,87 +74,76 @@ const Rules = ((P1, P2, Board) => {
         return currentPlayer
     }
 
-    const checkWin = (board) => {
+    const checkWin = () => {
        if (checkCol() || checkDia() || checkRow()) {
            return 'win'
-       } else if(checkTie()) {
-           console.log('tie')
-       }
+       } 
     }
 
     // if every tile data-player is not in board, means every tile is occupied
-    const checkTie = () => {
-        let board = [1,2,3,4,5,6,7,8,9]
-        let flag = false
-        tiles.forEach(item => {
-           if(board.includes(parseInt(item.getAttribute('data-player')))) {
-               flag = true
-               return 
-           }
-        return flag
-        })
-    }
 
     const checkRow = () => {
-        if (tiles[0].getAttribute('data-player') === tiles[1].getAttribute('data-player') &&
-            tiles[0].getAttribute('data-player') === tiles[2].getAttribute('data-player') ) {
-            return tiles[0].getAttribute('data-player');
-        } else if (tiles[3].getAttribute('data-player') === tiles[5].getAttribute('data-player') &&
-                    tiles[3].getAttribute('data-player') === tiles[4].getAttribute('data-player') ) {
-                    return tiles[3].getAttribute('data-player');
-        } else if (tiles[6].getAttribute('data-player') === tiles[7].getAttribute('data-player') &&
-                    tiles[6].getAttribute('data-player') === tiles[8].getAttribute('data-player')) {
-                    return tiles[6].getAttribute('data-player');
+        if (board[0] === board[1]&&
+            board[0] === board[2]) {
+                return board[0];
+        } else if (board[3] === board[4]&&
+            board[3] === board[5]) {
+                return board[3]
+        } else if (board[6] === board[7]&&
+            board[6] === board[8]) {
+                return board[6];
         }
     }
 
     const checkCol = () => {
-        if (tiles[0].getAttribute('data-player') === tiles[3].getAttribute('data-player') &&
-            tiles[0].getAttribute('data-player') === tiles[6].getAttribute('data-player') ) {
-                return tiles[0].getAttribute('data-player');
-        } else if (tiles[1].getAttribute('data-player') === tiles[4].getAttribute('data-player') &&
-                    tiles[1].getAttribute('data-player') === tiles[7].getAttribute('data-player') ) {
-                    return tiles[1].getAttribute('data-player');
-        } else if (tiles[2].getAttribute('data-player') === tiles[5].getAttribute('data-player') &&
-                    tiles[2].getAttribute('data-player') === tiles[8].getAttribute('data-player')) {
-                    return tiles[2].getAttribute('data-player');
+        if (board[0] === board[3]&&
+            board[0] === board[6]) {
+                return board[0];
+        } else if (board[1] === board[4]&&
+            board[1] === board[7]) {
+                return board[1];
+        } else if (board[2] === board[5]&&
+            board[2] === board[8]) {
+                return board[2];
         }
     }
 
     const checkDia = () => {
-        if (tiles[0].getAttribute('data-player') === tiles[4].getAttribute('data-player') &&
-            tiles[0].getAttribute('data-player') === tiles[8].getAttribute('data-player') ) {
-                return tiles[0].getAttribute('data-player');
-        } else if (tiles[2].getAttribute('data-player') === tiles[4].getAttribute('data-player') &&
-                    tiles[2].getAttribute('data-player') === tiles[6].getAttribute('data-player') ) {
-                        return tiles[2].getAttribute('data-player');
+        if (board[0] === board[4]&&
+            board[0] === board[8]) {
+                return board[0];
+        } else if (board[2] === board[4]&&
+            board[2] === board[6]) {
+                return board[2];
         } 
     }
 
     return {changeTurn, checkPlayer, checkWin};
 })('p1', 'p2', board.Board)
 
-DisplayBoard.display(GameBoard.Board);
+Display.display(GameBoard.Board);
 const tiles = document.querySelectorAll('.tile');
 
 tiles.forEach(item => {
     item.addEventListener('click', function() {
-        if (Rules.checkPlayer() === 'p1' && !(['p2', 'p1'].includes(item.getAttribute('data-player')))) {   //Aqui estaba evaluando rules.currentPlayer    
+        if (Rules.checkPlayer() === 'p1' && !(['p2', 'p1'].includes(item.getAttribute('data-player')))) { 
             item.style.backgroundColor = '#F8E9A1';
             item.setAttribute('data-player', 'p1');
+            GameBoard.Board[item.id - 1]= 'p1';
             if (Rules.checkWin()) {
-                alert('Player 1 has won');
-                DisplayBoard.resetBoard()
-                return
+                Display.resetBoard();
+                Display.endMessage('p1');
+                return;
             }
             Rules.changeTurn(); 
         } else if (Rules.checkPlayer() === 'p2' && !(['p2', 'p1'].includes(item.getAttribute('data-player')))) {
             item.style.backgroundColor = '#374785';
             item.setAttribute('data-player', 'p2');
+            GameBoard.Board[item.id - 1]= 'p2';
             if (Rules.checkWin()) {
-                alert('Player 2 has won')
-                DisplayBoard.resetBoard()
-                return
+                Display.resetBoard();
+                Display.endMessage('p2');
+                return;
             }
             Rules.changeTurn(); 
         }
@@ -150,7 +152,22 @@ tiles.forEach(item => {
 });
 
 const restartButton = document.getElementById('restartButton')
+const startButton = document.getElementById('startButton')
 
+startButton.addEventListener('click', function() {
+    const p1Name = document.getElementById('p1Name').value
+    const p2Name = document.getElementById('p2Name').value
+    if (p1Name !== '' && p2Name !== '') {
+        document.getElementById('p1').innerText = p1Name
+        document.getElementById('p2').innerText = p2Name
+        document.getElementById('startBox').classList.add('hidden')
+        document.getElementById('mainContainer').classList.remove('hidden')
+        document.getElementById('mainContainer').classList.add('visible')
+    } else {
+        alert("Player's names can't be empty")
+    }
+
+})
 restartButton.addEventListener('click', function() {
-    DisplayBoard.resetBoard()
+    Display.resetBoard()
 })
